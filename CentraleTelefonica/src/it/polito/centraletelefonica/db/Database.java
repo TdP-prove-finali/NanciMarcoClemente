1f5a1a1f888b5b4b1d41b25f83ba04468f853765
Golla1 <Golla1@localhost>
2017-06-27 14:41:59 +0200

    Database 1.5

--- CentraleTelefonica/src/it/polito/centraletelefonica/db/Database.java
+++ CentraleTelefonica/src/it/polito/centraletelefonica/db/Database.java
@@ -3,7 +3,11 @@ package it.polito.centraletelefonica.db;
 import java.sql.Connection;
 
 public interface Database {
-	
+
+	final String JDBC_URL = "JDBC_URL";
+	final String USERNAME = "USERNAME";
+	final String PASSWORD = "PASSWORD";
+
 	public Connection getConnection();
 
 }
--- CentraleTelefonica/src/it/polito/centraletelefonica/db/MySQLDatabase.java
+++ CentraleTelefonica/src/it/polito/centraletelefonica/db/MySQLDatabase.java
@@ -2,15 +2,19 @@ package it.polito.centraletelefonica.db;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class MySQLDatabase implements Database {
 
+	private static final String TYPE = "MY_SQL";
 	private ComboPooledDataSource dataSource;
 	private String dbName;
+	private Properties properties;
 
-	public MySQLDatabase(String dbName) {
+	public MySQLDatabase(String dbName, Properties properties) {
+		this.setProperties(properties);
 		this.setDbName(dbName);
 	}
 
@@ -22,23 +26,66 @@ public class MySQLDatabase implements Database {
 		this.dbName = dbName;
 	}
 
+	public Properties getProperties() {
+		return properties;
+	}
+
+	public void setProperties(Properties properties) {
+		this.properties = properties;
+	}
+
 	@Override
 	public Connection getConnection() {
 
 		if (dataSource == null) {
+			
 			dataSource = new ComboPooledDataSource();
-		}
+			dataSource.setJdbcUrl(properties.getProperty(Database.JDBC_URL));
+			dataSource.setUser(properties.getProperty(Database.USERNAME));
+			dataSource.setPassword(properties.getProperty(Database.PASSWORD));
+			
+			try {
+				return dataSource.getConnection();
+			}
 
-		try {
-			return dataSource.getConnection();
-		}
-
-		catch (SQLException e) {
-			// TODO: eccezione da gestire
-			e.printStackTrace();
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 
 		return null;
 	}
+
+	@Override
+	public String toString() {
+		return String.join(" ", dbName, TYPE);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dbName == null) ? 0 : dbName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MySQLDatabase other = (MySQLDatabase) obj;
+		if (dbName == null) {
+			if (other.dbName != null)
+				return false;
+		} else if (!dbName.equals(other.dbName))
+			return false;
+		return true;
+	}
+	
+	
 
 }
